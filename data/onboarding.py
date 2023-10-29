915d284badc738eb4163eab9151bb93456732f9a
NastyaGavrichenkova <lady_markos@mail.ru>
2023-10-29 12:10:14 +0700

    add tests for wikipedia

---
+++ data/onboarding.py
@@ -0,0 +1,4 @@
+text_on_page = {1: 'The Free Encyclopedia\n…in over 300 languages',
+                2: 'New ways to explore',
+                3: 'Reading lists with sync',
+                4: 'Send anonymous data'}
---
+++ tests/android_app/test_wikipedia.py
@@ -0,0 +1,51 @@
+import allure
+
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser, have
+
+from data import onboarding
+
+
+def test_run_onboarding():
+    page_number = 1
+
+    while page_number < 4:
+
+        with allure.step(f'Verify content on the {page_number} screen'):
+            text = browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView'))
+            text.should(have.text(onboarding.text_on_page[page_number]))
+
+        with allure.step(f'Open the {page_number + 2} onboarding screen'):
+            browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
+
+        page_number += 1
+
+
+def test_search():
+    with allure.step('Skip welcome screen'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
+
+    with allure.step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type('Appium')
+
+    with allure.step('Verify content found'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Appium'))
+
+
+def test_search_and_open_article():
+    with allure.step('Skip welcome screen'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
+
+    with allure.step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type('Appium')
+
+    with allure.step('Open the first article on the screen'):
+        browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')).first.click()
+
+    with allure.step('Verify content in the article'):
+        results = browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/view_wiki_error_text'))
+        results.should(have.text('An error occurred'))
